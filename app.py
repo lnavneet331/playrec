fa0c57bce8947d3252a245070424b56220ffdfe7
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-05-21 18:32:23 +0530

    maybe final

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ import pickle
 loaded_model = pickle.load(open('rf_model.sav', 'rb'))
 import pandas as pd
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Set up Spotify API credentials
-client_id = "client_id"
-client_secret = 'client_secret'
+client_id = os.getenv("client_id")
+client_secret = os.getenv('client_secret')
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -18,7 +21,6 @@ def get_song_details(song_name):
     if results['tracks']['items']:
         track = results['tracks']['items'][0]
         spotify_id = track['id']
-        st.write("Spotify ID: ", spotify_id)
         audio_features = sp.audio_features([spotify_id])[0]
         return spotify_id, audio_features
     else:
@@ -36,18 +38,16 @@ if song_name:
 
     if spotify_id and audio_features:
         # Display song details
-        # st.write(f"Spotify ID for '{song_name}': {spotify_id}")
-        # st.write("Audio Features:")
-        # st.write(audio_features)
-        # convert audio features to dataframe
+        st.write(f"Spotify ID for '{song_name}': {spotify_id}")
         audio_features = pd.DataFrame(audio_features, index=[0])
         audio_features = audio_features.drop(['key', 'mode', 'speechiness', 'type', 'id', "uri", "track_href", "analysis_url", "time_signature"], axis=1)
+        audio_features = audio_features[['duration_ms', 'acousticness', 'instrumentalness',  'liveness', 'loudness', 'danceability', 'energy', 'tempo', 'valence']]
         result = loaded_model.predict(audio_features)
         for i in result:
             if i == -1:
-                st.write("You probably won't like this song")
+                st.write("You probably won't like this song 🥺")
             elif i == 1:
-                st.write("You probably will like this song")
+                st.write("You probably will like this song 💖")
             else:
                 st.write("Not enough data to predict")
     else:
